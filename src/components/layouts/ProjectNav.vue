9463ccff2906baa3601474456db3c56d23261c08
Jinnie Cho <yejincho1105@gmail.com>
2021-10-20 20:24:46 +0900

    💄 Styled ProjectNav component for mobile

--- src/components/layouts/ProjectNav.vue
+++ src/components/layouts/ProjectNav.vue
@@ -5,16 +5,16 @@
         <div class="project-nav">
           <div class="project-nav-next">
             <div class="next-caption">
-              <h2 class="next-caption-text">Next</h2>
+              <h3 class="next-caption-text">next</h3>
               <i class="ic-arrow-md" aria-hidden="true" />
             </div>
             <div class="next-project">
               <h1 class="next-project-text">golden</h1>
             </div>
           </div>
-          <div class="project-nav-previous">
+          <div class="project-nav-previous lg-only">
             <div class="previous-caption">
-              <h2 class="previous-caption-text">Previous</h2>
+              <h3 class="previous-caption-text">previous</h3>
               <i class="ic-arrow-md" aria-hidden="true" />
             </div>
             <div class="previous-project">
@@ -28,7 +28,61 @@
 </template>
 
 <script>
-export default {}
+export default {
+  name: 'ProjectNav'
+}
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.project-nav {
+  margin: 0 -#{$sm-margin};
+  overflow: hidden;
+  background-color: $black;
+
+  &-next {
+    @include column-flex;
+    position: relative;
+    z-index: $footer-index;
+    height: 314px;
+
+    &::after {
+      content: '';
+      @include pos-center-y;
+      left: 120px;
+      width: 600px;
+      height: 600px;
+      z-index: $footer-circle-bg-index;
+      border-radius: 50%;
+      background-color: $icterine;
+    }
+
+    .next-caption {
+      @include flex;
+      margin-bottom: 30px;
+      z-index: $footer-index;
+
+      &-text {
+        @include text-style(h3-sub, $main-coral);
+        margin-right: 32px;
+        text-transform: capitalize;
+        mix-blend-mode: difference;
+      }
+
+      .ic-arrow-md {
+        display: block;
+        font-size: 30px;
+        mix-blend-mode: difference;
+        color: $main-cambridge-blue;
+        transform: rotate(-90deg);
+      }
+    }
+
+    .next-project {
+      &-text {
+        @include text-style(h1, $white);
+        mix-blend-mode: difference;
+      }
+    }
+  }
+}
+</style>
