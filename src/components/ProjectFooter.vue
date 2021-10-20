cee1eff43f7429f88783ed080b0d91e9e694ddd7
Jinnie Cho <yejincho1105@gmail.com>
2021-10-20 18:46:04 +0900

    💄 Styled ProjectFooter component for mobile

--- src/components/ProjectFooter.vue
+++ src/components/ProjectFooter.vue
@@ -34,7 +34,7 @@
             />
           </div>
 
-          <div class="contents-footer-captions">
+          <div class="project-footer-captions">
             <ul class="captions-list">
               <li class="made-with">
                 Made with lots of Kombucha🍹 and Love 💜
@@ -54,4 +54,61 @@ export default {
 }
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.project-footer {
+  @include column-flex;
+  margin: 0 -#{$sm-margin};
+  height: 520px;
+  overflow: hidden;
+  text-align: center;
+  background-color: $dutch-white;
+
+  &-links {
+    .links-list {
+      margin-bottom: 30px;
+
+      .list-title {
+        margin-bottom: 24px;
+
+        h6 {
+          @include text-style(link, $grey-3);
+        }
+      }
+
+      .list-item {
+        margin-bottom: 24px;
+
+        a {
+          @include text-style(link, $dutch-white);
+          mix-blend-mode: difference;
+        }
+      }
+    }
+  }
+
+  &-wave {
+    @include flex;
+    width: 162px;
+    height: 10px;
+    margin-bottom: 30px;
+    overflow: hidden;
+    text-align: center;
+
+    img {
+      display: block;
+      height: 100%;
+      overflow: hidden;
+      object-fit: cover;
+    }
+  }
+
+  &-captions {
+    .captions-list {
+      .made-with,
+      .copyright {
+        @include text-style(caption, $grey-1);
+      }
+    }
+  }
+}
+</style>
